6af54010804efe754fdcda8c5563be8410e40e77
Richard Skogeby <36446048+rSkogeby@users.noreply.github.com>
2018-03-06 11:09:14 +0100

    lesson-02: List view using basic adapter

--- app/src/main/java/rskogeby/com/coderswag/controller/MainActivity.kt
+++ app/src/main/java/rskogeby/com/coderswag/controller/MainActivity.kt
@@ -2,12 +2,22 @@ package rskogeby.com.coderswag.controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 import rskogeby.com.coderswag.R
+import rskogeby.com.coderswag.model.Category
+import rskogeby.com.coderswag.services.DataService
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var adapter: ArrayAdapter<Category>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                DataService.categories)
+        categoryListView.adapter = adapter
     }
 }
--- app/src/main/java/rskogeby/com/coderswag/model/Category.kt
+++ app/src/main/java/rskogeby/com/coderswag/model/Category.kt
@@ -3,4 +3,8 @@ package rskogeby.com.coderswag.model
 /**
  * Created by richa on 2018-03-06.
  */
-class Category(val name: String,val image: String)
+class Category(val title: String,val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
