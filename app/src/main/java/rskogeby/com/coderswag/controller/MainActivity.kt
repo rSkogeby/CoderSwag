0959cce33d103f275432c1d77d102d01a8e4fde3
Richard Skogeby <36446048+rSkogeby@users.noreply.github.com>
2018-03-06 14:04:36 +0100

    lesson-63 Recyclerview added to gradle script

--- app/src/main/java/rskogeby/com/coderswag/controller/MainActivity.kt
+++ app/src/main/java/rskogeby/com/coderswag/controller/MainActivity.kt
@@ -3,6 +3,7 @@ package rskogeby.com.coderswag.controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import rskogeby.com.coderswag.R
 import rskogeby.com.coderswag.adapters.CategoryAdapter
@@ -19,5 +20,10 @@ class MainActivity : AppCompatActivity() {
         adapter = CategoryAdapter(this,
                 DataService.categories)
         categoryListView.adapter = adapter
+
+        categoryListView.setOnItemClickListener { adapterView, view, i, l ->
+            val category = DataService.categories[i]
+            Toast.makeText(this,"You clicked on the ${category.title} cell",Toast.LENGTH_SHORT).show()
+        }
     }
 }
